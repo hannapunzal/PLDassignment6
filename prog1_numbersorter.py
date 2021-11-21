917133d32f9d12d5f8fba32034868eed69cdcc37
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-11-21 12:28:10 +0800

    Program 1 step 1 (ask for four numbers) done.

---
+++ prog1_numbersorter.py
@@ -0,0 +1,17 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+#ask four numbers
+firstNum = float(input("Enter first number: "))
+secondNum = float(input("Enter second number: "))
+thirdNum = float(input("Enter third number: "))
+fourthNum = float(input("Enter fourth number: "))
+
+#print four numbers from highest to lowest
+
+#combinations:
+# 1234, 1243, 1324, 1342, 1423, 1432 
+# 2134, 2143, 2314, 2341, 2413, 2431
+# 3124, 3142, 3214, 3241, 3412, 3421 
+# 4123, 4132, 4213, 4231, 4312, 4321
